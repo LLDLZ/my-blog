b1dbd3143c0ca43922d082c8b829923625425f0c
LLDLZ <792866833@qq.com>
2023-05-04 15:47:34 +0800

    vuex 解析完成

--- docs/.vuepress/menu/blog/vuexCodeAnalysis.js
+++ docs/.vuepress/menu/blog/vuexCodeAnalysis.js
@@ -4,15 +4,11 @@ module.exports = [
     path: "/blog/vuexCodeAnalysis/1",
   },
   {
-    title: "Vuex 源码解析（二）install 、 store 初始化 、 模块安装 、 state ... 实现原理",
+    title: "Vuex 源码解析（二）install 、 store 初始化 、 模块安装 、 state 、getters 实现原理",
     path: "/blog/vuexCodeAnalysis/2",
   },
   {
-    title: "Vuex 源码解析（三）",
+    title: "Vuex 源码解析（三）commit 、 dispatch 实现 、辅助函数实现",
     path: "/blog/vuexCodeAnalysis/3",
-  },
-  {
-    title: "Vuex 源码解析（四）",
-    path: "/blog/vuexCodeAnalysis/4",
-  },
+  }
 ];
