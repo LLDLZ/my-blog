ffebd5e6ebaceca6054b29cb116da20dd0fa2494
LLDLZ <792866833@qq.com>
2023-06-02 09:50:18 +0800

    目录名称优化

--- docs/.vuepress/menu/blog/writeAccordingPromiseA+.js
+++ docs/.vuepress/menu/blog/writeAccordingPromiseA+.js
@@ -1,11 +1,18 @@
 module.exports = [
   {
-    title: "手写 promise （ 一 ）",
+    title: "手写 promise （ 一 ）A+ 规范介绍以及运行 Promise 测试脚本",
     path: "/blog/writeAccordingPromiseA+/1",
   },
-
-    {
-        title: "手写 promise （ 二 ）",
-        path: "/blog/writeAccordingPromiseA+/2"
-    },
-    ];
+  {
+    title: "手写 promise （ 二 ）根据A+规范手写 Promise",
+    path: "/blog/writeAccordingPromiseA+/2",
+  },
+  {
+    title: "手写 promise （ 三 ） Promise 部分 API 实现",
+    path: "/blog/writeAccordingPromiseA+/3",
+  },
+  {
+    title: "async await 终极解决方案",
+    path: "/blog/writeAccordingPromiseA+/4",
+  },
+];
