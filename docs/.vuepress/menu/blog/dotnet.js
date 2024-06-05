1fab1372e730b27aaedc1e6b54e865113890b4c8
DLLZ <792866833@qq.com>
2024-06-05 21:26:03 +0800

    dotnet 面试题完成

--- docs/.vuepress/menu/blog/dotnet.js
+++ docs/.vuepress/menu/blog/dotnet.js
@@ -1,6 +1,10 @@
 module.exports = [
-    {
-        title: ".net efcore vue3 博客（一）",
-        path: "/blog/dotnet/1"
-    }
-]
+  {
+    title: "dotnet 面试题",
+    path: "/blog/dotnet/2",
+  },
+  {
+    title: ".net efcore vue3 博客（一）",
+    path: "/blog/dotnet/1",
+  },
+];
